10ee8722827a95140be969794b2d98701eef2347
Tonk Akshay Kumar <akshayverma79936>
2023-03-26 00:14:32 +0530

    3screens

--- App.jsx
+++ App.jsx
@@ -6,28 +6,34 @@ import PersonalDetailsRegistration from './screens/Registration/PersonalDetailsR
 import OtpKey from './screens/OTP/OtpKey';
 import OtpVerification from './screens/OTP/OtpVerification';
 import VerificationRegistration from './screens/Registration/VerificationRegistration';
+import Dashboard from './screens/DashBoard/Dashboard';
+import Summary from './screens/DashBoard/Summary';
+import SummaryAppointment from './screens/DashBoard/SummaryAppointment';
 const App = () => {
   return (
     <SafeAreaView>
-      <LinearGradient colors={['#1e1262', '#4d0d7e']} style={styles.gradient}>
+      {/* <LinearGradient colors={['#1e1262', '#4d0d7e']} style={styles.gradient}> */}
         {/* <PersonalDetailsRegistration /> */}
         {/* <MainRegistration /> */}
         {/* <Verification /> */}
         {/* <OtpKey /> */}
         {/* <OtpVerification /> */}
-        <VerificationRegistration />
-      </LinearGradient>
+        {/* <VerificationRegistration /> */}
+        {/* <Dashboard/> */}
+        {/* <Summary/> */}
+        <SummaryAppointment/>
+      {/* </LinearGradient> */}
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  gradient: {
-    height: '100%',
-    width: '100%',
-    padding: 10,
-    position: 'relative',
-  },
-});
+// const styles = StyleSheet.create({
+//   gradient: {
+//     height: '100%',
+//     width: '100%',
+//     padding: 10,
+//     position: 'relative',
+//   },
+// });
 
 export default App;
